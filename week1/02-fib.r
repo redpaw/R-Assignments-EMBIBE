7a4814ce6ff128302277363d8287ade94017b60d
pravin-sharma <pravinsharma007@gmail.com>
2014-06-08 20:15:43 +0530

    Fibonacci works

--- week1/02-fib.r
+++ week1/02-fib.r
@@ -1,9 +1,8 @@
 fib <- function(n, a, b, fib) {
   if (n==0) {
-    return fib
-  }
-  else {
-    append(fib,c(a));
-    return fib(n-1, b, a+b, fib);
+    return(fib)
+  } else {
+    fib <- union(fib,c(a))
+    return(fib(n-1, b, a+b, fib))
   }
 }
