05cfc110d6c5c5c9c7ab416895c37a9711598041
pravin-sharma <pravinsharma007@gmail.com>
2014-06-08 20:44:37 +0530

    Adding the iterative version

--- week1/02-fib.r
+++ week1/02-fib.r
@@ -1,8 +1,20 @@
-fib <- function(n, a, b, fib) {
+fib_recur <- function(n, a, b, fib) {
   if (n==0) {
     return(fib)
   } else {
     fib <- union(fib,c(a))
     return(fib(n-1, b, a+b, fib))
   }
+}
+
+fib_iter <- function(n) {
+  a <- 0; b<- 1; list <- c(); 
+  while (n>0) {
+    list <- union(list,c(a));
+    temp <- b;
+    b <- a + b;
+    a <- temp;    
+    n <- n-1; 
+  }
+   return(list)
 }
